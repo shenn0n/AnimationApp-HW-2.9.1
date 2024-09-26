e31d3824b0cfec16c911a5e8b28d5540bdb35c8a
shenn0n <shenn0n4412@gmail.com>
2024-09-26 15:16:43 +0300

    Initial Commit

---
+++ AnimationApp HW 2.9.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp HW 2.9.1
+//
+//  Created by Александр Манжосов on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
