8951f161e4b584ab12d175cd48886a0d7567d580
shenn0n <shenn0n4412@gmail.com>
2024-09-26 17:35:01 +0300

    Done

---
+++ AnimationApp HW 2.9.1/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  AnimationApp HW 2.9.1
+//
+//  Created by Александр Манжосов on 26.09.2024.
+//
+
+import UIKit
+
+struct Animation {
+    
+    let animation: String
+    let curve: String
+    let duration: CGFloat
+
+}
--- AnimationApp HW 2.9.1/ViewController.swift
+++ AnimationApp HW 2.9.1/ViewController.swift
@@ -5,15 +5,57 @@
 //  Created by Александр Манжосов on 26.09.2024.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var springView: SpringView!
+    @IBOutlet var springButton: SpringButton!
+    @IBOutlet var animationLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    
+    var animations: [Animation] = []
+    var countAnim: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animations = DataManager.shared.loadAnimations()
+        springView.isHidden = true
+        springView.layer.cornerRadius = 10
+        springButton.layer.cornerRadius = 10
+        springButton.setTitle("Run Animation", for: .normal)
     }
 
+    @IBAction func startSpringBatton(_ sender: SpringButton) {
+        
+        
+        
+        if countAnim < 3 {
+            springView.isHidden = false
+            updateAnimation(anim: animations[countAnim])
+            countAnim += 1
+        } else {
+            updateAnimation(anim: animations[countAnim])
+            countAnim = 0
+        }
+        updateButtonTitle(count: countAnim)
+    }
+    
+}
 
+extension ViewController {
+    func updateAnimation(anim: Animation) {
+        springView.animation = anim.animation
+        springView.curve = anim.curve
+        springView.duration = anim.duration
+        animationLabel.text = "Animation: \(anim.animation)"
+        curveLabel.text = "Curve: \(anim.curve)"
+        durationLabel.text = "Duration: \(String(Float(anim.duration)))"
+        springView.animate()
+    }
+    func updateButtonTitle(count: Int) {
+        springButton.setTitle("Run \(animations[count].animation)", for: .normal)
+    }
 }
 
